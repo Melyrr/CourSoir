416565c511ff5312e682e0f0faa4b65bf9bcd9a4
Adnan EL KHARRAZ <2412adelkharraz@student.epfc.eu>
2022-10-17 20:26:11 +0200

    3 cercles mais avancé

--- src/main/java/com/example/ayaa/HelloApplication.java
+++ src/main/java/com/example/ayaa/HelloApplication.java
@@ -13,14 +13,18 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         int size = 100;
-        Canvas canvas = new Canvas(300, 300);
+        Canvas canvas = new Canvas(500, 300);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        gc.setFill(Color.color(1,1,0));
-        gc.fillOval(0, 10, size, size);
-        gc.setFill(Color.color(0,1,1));
-        gc.fillOval(100, 100, size, size);
-        gc.setFill(Color.color(1,0,1));
-        gc.fillOval(200, 200, size, size);
+        Circle[] circles = {
+                new Circle(Color.color(1, 1, 0), 0, 0, 100),
+                new Circle(Color.color(0, 1, 1), 100, 100, 100),
+                new Circle(Color.color(1, 0, 1), 200, 200, 100)
+        };
+
+        for (Circle c : circles) {
+            gc.setFill(c.color());
+            gc.fillOval(c.x(), c.y(), c.radius(), c.radius());
+        }
         stage.setTitle("Jouons avec RGB.");
         stage.setScene(new Scene(new VBox(canvas)));
         stage.show();
