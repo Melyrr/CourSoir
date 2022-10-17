6f5799358fb566b4a570f195044cc919b4a8ddc0
Adnan EL KHARRAZ <2412adelkharraz@student.epfc.eu>
2022-10-17 20:22:42 +0200

    juste des cercles tout con

--- src/main/java/com/example/ayaa/HelloApplication.java
+++ src/main/java/com/example/ayaa/HelloApplication.java
@@ -12,26 +12,16 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Canvas canvas = new Canvas(500, 500);
+        int size = 100;
+        Canvas canvas = new Canvas(300, 300);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-
-        FormParameters[] parameters = {
-                new FormParameters(false, Color.DARKGOLDENROD, 100, 120, 400),
-                new FormParameters(true, Color.DARKCYAN, -50, -50, 500),
-                new FormParameters(true, Color.DARKMAGENTA, 200, 200, 250),
-                new FormParameters(false, Color.DARKBLUE, 80, 100, 150)
-        };
-
-        for (FormParameters p : parameters) {
-            gc.setFill(p.color());
-            if (p.isCircle()){
-                gc.fillOval(p.x(),p.y(),p.size(),p.size());
-            }else{
-                gc.fillRect(p.x(),p.y(),p.size(),p.size());
-            }
-        }
-
-        stage.setTitle("N cercle ou carré");
+        gc.setFill(Color.color(1,1,0));
+        gc.fillOval(0, 10, size, size);
+        gc.setFill(Color.color(0,1,1));
+        gc.fillOval(100, 100, size, size);
+        gc.setFill(Color.color(1,0,1));
+        gc.fillOval(200, 200, size, size);
+        stage.setTitle("Jouons avec RGB.");
         stage.setScene(new Scene(new VBox(canvas)));
         stage.show();
     }
