1bfebd03fc9e236058ed5efc0b2d987b2dc1b3e0
Melyrr <57968@etu.he2b.be>
2022-10-24 21:26:29 +0200

    Rectangles imbriqués dans des cercles et vis versa.

--- src/main/java/com/example/ayaa/HelloApplication.java
+++ src/main/java/com/example/ayaa/HelloApplication.java
@@ -17,45 +17,21 @@ public class HelloApplication extends Application {
         Canvas canvas = new Canvas(800, 800);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        gc.setFill(Color.BLACK);
-        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
-            double width = 400;
-            double a = 0;
-            for (int i = 0; i < 20; ++i) {
-                gc.setFill(Color.color(Math.random(), Math.random(), Math.random()));
-                gc.fillRect(a, a, width, width);
-                width = width - 25;
-                a = a + 12.5;
-            }
-            double b=400;
-            double z = 400;
-            for (int i = 0; i < 20; ++i) {
-                gc.setFill(Color.color(Math.random(), Math.random(), Math.random()));
-                gc.fillRect(b, b, z, z);
-                z = z - 25;
-                b = b + 12.5;
-            }
-            double az = 400;
-            double av = 400;
-            double am = 0;
-            for (int i = 0; i < 20; ++i) {
-                gc.setFill(Color.color(Math.random(), Math.random(), Math.random()));
-                gc.fillRect(0+am, av, az, az);
-                az = az - 25;
-                av = av + 12.5;
-                am= am + 12.5;
-            }
-        double ak = 400;
-        double al = 400;
-        double aj = 0;
-        for (int i = 0; i < 20; ++i) {
-            gc.setFill(Color.color(Math.random(), Math.random(), Math.random()));
-            gc.fillRect(al, 0+aj, ak, ak);
-            ak = ak - 25;
-            al = al + 12.5;
-            aj= aj + 12.5;
-        }
+
+        gc.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        gc.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+        gc.fillOval(0,0,800,800);
+        gc.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+        gc.fillRect(118,115,560, 570);
+        canvas.setOnMouseMoved(event -> {
+                    System.out.println(event.getX() + " " + event.getY());
+                });
+
+
+
+
         stage.setScene(new Scene(new VBox(canvas)));
         stage.setTitle("Rectangle inmbriqué avec des couleurs différentes*4");
         stage.show();
