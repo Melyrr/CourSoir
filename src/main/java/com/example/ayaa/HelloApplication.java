01e566e926e1a28d100acd61fb2f73b6653f20b7
Melyrr <57968@etu.he2b.be>
2022-10-24 18:28:39 +0200

    Simple carré sans record

--- src/main/java/com/example/ayaa/HelloApplication.java
+++ src/main/java/com/example/ayaa/HelloApplication.java
@@ -13,28 +13,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         int size = 100;
-        Canvas canvas = new Canvas(500, 150);
+        Canvas canvas = new Canvas(400, 400);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        Circle[] circles = {
-                new Circle(Color.rgb(255, 0, 0),          0,   0, 45),
-                new Circle(Color.rgb(213, 16, 17),          50,   0, 45),
-                new Circle(Color.rgb(170, 23, 23), 100, 0, 45),
-                new Circle(Color.rgb(112, 65, 6), 150, 0, 45),
-                new Circle(Color.rgb(86, 73, 14), 200, 0, 45),
-                new Circle(Color.rgb(77, 94, 17), 250, 0, 45),
-                new Circle(Color.rgb(160, 180, 53), 300, 0, 45),
-                new Circle(Color.rgb(140, 190, 40), 400, 0, 45),
-                new Circle(Color.rgb(100, 230, 60), 450, 0, 45),
-                new Circle(Color.rgb(172, 255, 0), 500, 0, 45),
-
-        };
-        for(int i = 0; i < 5; ++i) {
-            for (Circle c : circles) {
-                gc.setFill(c.color());
-                gc.fillOval(c.x(), c.y(), c.radius(), c.radius());
-            }
-        }
-        stage.setTitle("Color RGB red-green-blue");
+        gc.setFill(Color.rgb(1, 1, 0));
+        gc.fillRect(50, 50, 300, 300);
+        stage.setTitle("Carré RGB");
         stage.setScene(new Scene(new VBox(canvas)));
         stage.show();
     }
