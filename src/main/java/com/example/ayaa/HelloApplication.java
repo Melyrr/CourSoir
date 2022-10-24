15a2d8bbc80b907d69802643f0fd50fc4a0f269e
Melyrr <57968@etu.he2b.be>
2022-10-24 19:48:15 +0200

    cercle couleur différentes en fonction de leur position

--- src/main/java/com/example/ayaa/HelloApplication.java
+++ src/main/java/com/example/ayaa/HelloApplication.java
@@ -13,17 +13,22 @@ import java.lang.Math;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        double width = 20;
+        double radius = 20;
         Canvas canvas = new Canvas(500, 500);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        gc.setFill(Color.LIGHTYELLOW);
-        gc.fillRect(0,0,500,500);
-        for(int i = 0; i<500; i++){
-            double value = Math.random();
-            gc.setFill(Color.PURPLE);
-            double x = Math.random() * canvas.getWidth();
-            double y = Math.random() * canvas.getHeight();
-            gc.fillOval(x,y,width,width);
+
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        for (int i = 0; i < 500; ++i) {
+            double x = Math.random() * (canvas.getWidth() - radius);
+            double y = Math.random() * (canvas.getHeight() - radius);
+            if (y <= canvas.getHeight() / 2 - radius / 2) {
+                gc.setFill(Color.GOLD);
+            } else {
+                gc.setFill(Color.ORANGE);
+            }
+            gc.fillOval(x, y, radius, radius);
         }
 
         stage.setScene(new Scene(new VBox(canvas)));
