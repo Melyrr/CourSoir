ba997d745fcf4442c395115d3394e93e00577b5c
Melyrr <57968@etu.he2b.be>
2022-10-24 19:11:02 +0200

    500 cercles au hasard

--- src/main/java/com/example/ayaa/HelloApplication.java
+++ src/main/java/com/example/ayaa/HelloApplication.java
@@ -8,36 +8,26 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.paint.Color;
 import javafx.scene.layout.VBox;
 import java.io.IOException;
+import java.lang.Math;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        int size = 100;
-        Canvas canvas = new Canvas(400, 400);
+        double width = 20;
+        Canvas canvas = new Canvas(500, 500);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-
-        gc.setFill(Color.BLUE);
-        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight() / 2);
-        gc.setFill(Color.PURPLE);
-        gc.fillRect(0, canvas.getHeight() / 2, canvas.getWidth(), canvas.getHeight());
-
-        canvas.setOnMouseMoved(event -> {
-            System.out.println(event.getX() + " " + event.getY());
-            if (event.getY() < canvas.getHeight() / 2) {
-                gc.setFill(Color.AQUA);
-                gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight() / 2);
-                gc.setFill(Color.PURPLE);
-                gc.fillRect(0, canvas.getHeight() / 2, canvas.getWidth(), canvas.getHeight());
-            } else {
-                gc.setFill(Color.BLUE);
-                gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight() / 2);
-                gc.setFill(Color.PINK);
-                gc.fillRect(0, canvas.getHeight() / 2, canvas.getWidth(), canvas.getHeight());
-            }
-        });
+        gc.setFill(Color.LIGHTYELLOW);
+        gc.fillRect(0,0,500,500);
+        for(int i = 0; i<500; i++){
+            double value = Math.random();
+            gc.setFill(Color.PURPLE);
+            double x = Math.random() * canvas.getWidth();
+            double y = Math.random() * canvas.getHeight();
+            gc.fillOval(x,y,width,width);
+        }
 
         stage.setScene(new Scene(new VBox(canvas)));
-        stage.setTitle("demo mouses");
+        stage.setTitle("Cercle placé au hasard");
         stage.show();
     }
     public static void main(String[] args) {
