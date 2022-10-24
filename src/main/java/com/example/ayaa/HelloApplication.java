6075e63cb087c79c281d2c95a9cf4ece581a3a98
Melyrr <57968@etu.he2b.be>
2022-10-24 21:03:34 +0200

    Rectangles imbriqués dans 4 zones

--- src/main/java/com/example/ayaa/HelloApplication.java
+++ src/main/java/com/example/ayaa/HelloApplication.java
@@ -14,23 +14,50 @@ import java.util.Random;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        double width = 500;
-        double a = 0;
-        Canvas canvas = new Canvas(500, 500);
+        Canvas canvas = new Canvas(800, 800);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
         gc.setFill(Color.BLACK);
         gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
+            double width = 400;
+            double a = 0;
+            for (int i = 0; i < 20; ++i) {
+                gc.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+                gc.fillRect(a, a, width, width);
+                width = width - 25;
+                a = a + 12.5;
+            }
+            double b=400;
+            double z = 400;
+            for (int i = 0; i < 20; ++i) {
+                gc.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+                gc.fillRect(b, b, z, z);
+                z = z - 25;
+                b = b + 12.5;
+            }
+            double az = 400;
+            double av = 400;
+            double am = 0;
+            for (int i = 0; i < 20; ++i) {
+                gc.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+                gc.fillRect(0+am, av, az, az);
+                az = az - 25;
+                av = av + 12.5;
+                am= am + 12.5;
+            }
+        double ak = 400;
+        double al = 400;
+        double aj = 0;
         for (int i = 0; i < 20; ++i) {
             gc.setFill(Color.color(Math.random(), Math.random(), Math.random()));
-            gc.fillRect(a,a,width,width);
-            width=width-25;
-            a=a+12.5;
+            gc.fillRect(al, 0+aj, ak, ak);
+            ak = ak - 25;
+            al = al + 12.5;
+            aj= aj + 12.5;
         }
-
         stage.setScene(new Scene(new VBox(canvas)));
-        stage.setTitle("Rectangle inmbriqué avec des couleurs différentes");
+        stage.setTitle("Rectangle inmbriqué avec des couleurs différentes*4");
         stage.show();
     }
     public static void main(String[] args) {
